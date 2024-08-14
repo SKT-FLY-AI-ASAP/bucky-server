83a1cb6b9f5dd6d5d3c27936f128c725a137cd6f
Yusun Choi <nsuy.ch@gmail.com>
2024-08-14 11:03:06 +0900

    refactor: Change directory structure (#1)

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ from typing import Optional
 from pathlib import Path
 
 class Settings(BaseSettings):
-    BASE_URL: Optional[str] = None
     PORT: Optional[int] = None
-    DB_URL: Optional[str] = None
+    DB_HOST: Optional[str] = None
+    DB_PORT: Optional[int] = None
     DB_USER: Optional[str] = None
     DB_PASSWORD: Optional[str] = None
+    DB_NAME: Optional[str] = None
 
     class Config:
         env_file = Path(__file__).parent / '.env'
