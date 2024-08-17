46b6dd8c88607fb56e50c5b97bb26893b9e666af
Yusun Choi <nsuy.ch@gmail.com>
2024-08-17 22:36:05 +0900

    feat: Create table from metadata (#1)

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from config import settings
+from base import Base
+from src.user.models import User
 
 DB_URL = f'mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 
 class engineconn:
     def __init__(self):
-        self.engine = create_engine(DB_URL, pool_recycle=500)
+        self.engine = create_engine(DB_URL, echo=True, pool_recycle=500)
 
     def sessionmaker(self):
         Session = sessionmaker(bind=self.engine)
@@ -16,3 +18,7 @@ class engineconn:
     def connection(self):
         conn = self.engine.connect()
         return conn
+
+    def create_tables(self):
+        print("Creating tables...")
+        Base.metadata.create_all(bind=self.engine)
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import uvicorn
 from fastapi import FastAPI
-from config import settings
 from database import engineconn
 
 app = FastAPI()
+engine = engineconn()
+session = engine.sessionmaker()
 
 def check_connection():
-    engine = engineconn().engine
     try:
-        with engine.begin() as connection:
-            print(f"DB connected. >>> {engine.url}")
+        with engine.engine.begin() as connection:
+            print(f"DB connected. >>> {engine.engine.url}")
+            engine.create_tables()
     except Exception as e:
         print(f"DB connection failed.\n{e}")
 
