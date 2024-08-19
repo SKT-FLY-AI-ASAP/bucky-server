0084d5b63fba3857f6d77eab285db4795dc1d319
Yusun Choi <nsuy.ch@gmail.com>
2024-08-19 19:24:19 +0900

    chore: Add base custom exception (#1)

--- core/exceptions.py
+++ core/exceptions.py
@@ -10,6 +10,12 @@ class BaseCustomException(Exception):
     def __str__(self):
         return self.detail
 
+# Base custom exception
+async def base_custom_exception_handler(request: Request, exc: BaseCustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
 
 # Value Error
 async def value_error_handler(request: Request, exc: ValueError):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from core.database import engineconn
 from core.redis_config import redis_config
 
-from core.exceptions import value_error_handler, generic_exception_handler
+from core.exceptions import value_error_handler, generic_exception_handler, BaseCustomException, base_custom_exception_handler
 
 from src.user.router import router as user_router
 
@@ -14,6 +14,7 @@ rd = redis_config()
 # Exception handler
 app.add_exception_handler(ValueError, value_error_handler)
 app.add_exception_handler(HTTPException, generic_exception_handler)
+app.add_exception_handler(BaseCustomException, base_custom_exception_handler)
 
 # Router
 app.include_router(user_router)
