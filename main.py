1f594f0be8a3bda3bb9c4dcca348e76ed8b17390
Yusun Choi <nsuy.ch@gmail.com>
2024-08-19 19:04:25 +0900

    feat: Add custom exception (#1)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from core.database import engineconn
 from core.redis_config import redis_config
+
+from core.exceptions import value_error_handler, generic_exception_handler
 
 from src.user.router import router as user_router
 
@@ -8,6 +10,10 @@ app = FastAPI()
 engine = engineconn()
 session = engine.sessionmaker()
 rd = redis_config()
+
+# Exception handler
+app.add_exception_handler(ValueError, value_error_handler)
+app.add_exception_handler(HTTPException, generic_exception_handler)
 
 # Router
 app.include_router(user_router)
