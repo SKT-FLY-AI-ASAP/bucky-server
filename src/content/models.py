63068b2b8eb5934a4e7d3fc9a24cad8e906d573e
Yusun Choi <nsuy.ch@gmail.com>
2024-08-22 11:01:38 +0900

    feat: Add Content & Design models (#3)

--- src/content/models.py
+++ src/content/models.py
@@ -20,3 +20,37 @@ class Sketch(Base):
     user = relationship('User', back_populates='sketch')
 
 User.sketch = relationship('Sketch', back_populates='user')
+
+
+class Content(Base):
+    __tablename__ = "content"
+
+    content_id = Column(BigInteger, primary_key=True, nullable=False, autoincrement=True)
+    content_title = Column(String(50), nullable=False)
+    content_type = Column(Boolean, default=False)
+    content_url = Column(String(256), nullable=False)
+    is_removed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True))
+    removed_at = Column(DateTime(timezone=True))
+    user_id = Column(BigInteger, ForeignKey('user.user_id'))
+
+    user = relationship('User', back_populates='content')
+
+User.content = relationship('Content', back_populates='content')
+
+
+class Design(Base):
+    __tablename__ = "design"
+
+    design_id = Column(BigInteger, primary_key=True, nullable=False, autoincrement=True)
+    design_url = Column(String(256), nullable=False)
+    is_removed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True))
+    removed_at = Column(DateTime(timezone=True))
+    content_id = Column(BigInteger, ForeignKey('content.content_id'))
+
+    content = relationship('Content', back_populates='design')
+
+Content.design = relationship('Design', back_populates='design')
